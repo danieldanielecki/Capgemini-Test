a2e090c2f7811298107ea2ba5737d71c2a3f85b1
Daniel Danielecki <daniel.danielecki@mail.com>
2020-09-18 08:07:26 +0200

    Delete: unnecessary sendVote() method in DataService

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -7,16 +7,9 @@ export class DataService {
   private messageSource = new BehaviorSubject([]);
   currentMessage = this.messageSource.asObservable();
 
-  private messageSource2 = new BehaviorSubject({});
-  currentMessage2 = this.messageSource2.asObservable();
-
   constructor() {}
 
   sendVotes(votes: Vote[]) {
     this.messageSource.next(votes);
-  }
-
-  sendVote(vote: Vote) {
-    this.messageSource2.next(vote);
   }
 }
